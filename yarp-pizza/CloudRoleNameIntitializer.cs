952c547b76f07d956da441318c08bbf35fd57936
Shayne Boyer <spboyer@live.com>
2021-05-14 19:55:09 -0400

    appinsights fix

---
+++ yarp-pizza/CloudRoleNameIntitializer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace yarp_pizza
+{
+    public class CloudRoleNameInitializer : ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            telemetry.Context.Cloud.RoleName = "yarp-pizza";
+        }
+    }
+}
